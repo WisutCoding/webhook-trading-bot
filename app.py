a005ab3100b37d2f3d576ef5258d39225f78f744
unknown <wisut.kua@gmail.com>
2022-03-07 16:10:51 +0700

    24

--- app.py
+++ app.py
@@ -4,15 +4,10 @@ from flask import Flask, request, render_template
 from binance.client import Client
 from binance.enums import *
 import pandas as pd
-# import math
-# import decimal
 #---------------------------------------------------------------------------------
 app = Flask(__name__)
 client = Client(config.API_KEY, config.API_SECRET)
-#---------------------------------------------------------------------------------
-# def round_down(n, decimals=0):
-#     multiplier = 10 ** decimals
-#     return math.floor(n * multiplier) / multiplier
+
 #---------------------------------------------------------------------------------
 def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
     try:
@@ -24,55 +19,63 @@ def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
         return False
 
     return order
-
 #---------------------------------------------------------------------------------
 @app.route("/")
 def welcome():
     return render_template('index.html')
 #---------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------
 @app.route('/webhook', methods=['POST'])
 def webhook():
-
     #---------------------------------------------------
-    # Check Balance
-
-    account_info= client.get_account()
-    df = pd.DataFrame(account_info['balances'])
-
-    usdt_info = df.loc[df['asset']== 'USDT']
-    usdt_amount = round(pd.to_numeric(usdt_info['free'].values[0])*0.99,5)
-
-    btc_info = df.loc[df['asset']== 'BTC']
-    btc_amount = round(pd.to_numeric(btc_info['free'].values[0])*0.99,5)
-
-    #---------------------------------------------------
-    # Data from Webhook
-    data = json.loads(request.data)
+    # Get Webhook
+    data = json.loads(request.data)                         # Get json
     if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
         return{
             "code":"error",
             "message":"invalid passpharse"
         }
-
-    # BUY/SELL
-    order_action = data['strategy']['order_action'].upper() # BUY / SELL
-    symbol = data['ticker']
-    price = data['bar']['close']
     
+    order_action = data['strategy']['order_action'].upper() # Get BUY/SELL
+
+    symbol = data['ticker']                                 # Get Symbol
+
+    price = data['bar']['close']                            # Get Close Price
+
+    #---------------------------------------------------
+    # Check Balance
+
+    account_info= client.get_account()
+
+    df = pd.DataFrame(account_info['balances'])
+
+    usdt_info = df.loc[df['asset']== 'USDT']
+    usdt_amount = pd.to_numeric(usdt_info['free'].values[0])
+
+    btc_info = df.loc[df['asset']== 'BTC']
+    btc_amount = pd.to_numeric(btc_info['free'].values[0])
+
+    #---------------------------------------------------
+    # Money Management
+
+    bet_ratio = 0.25
+
+    buy_btc_amt = (bet_ratio*usdt_amount/close_price)*100000//1/100000    #>> 0.xxxxx
+    sell_btc_amt = (bet_ratio*btc_amount)*100000//1/100000                  #>> 0.xxxxx
+
+    # identify BUY/SELL amount
     if order_action == "BUY":
-        amount = 0.0005 # usdt_amount/price
+        amount = buy_btc_amt
     else:
-        amount = 0.0005
+        amount = sell_btc_amt
 
-
-
+    #---------------------------------------------------
     print('passphrase : ',data['passphrase'])
     print('time : ',data['time'])
-    print('ticker : ',data['ticker'])
-    print('bar : ',data['bar']['close'])
+    print('symbol >> ',symbol)
+    print('Price : ',data['bar']['close'])
     print('side >> ',order_action)
     print('amount >> ',amount)
-    print('symbol >> ',symbol)
 
     order_response = order(order_action, amount, symbol)   # Minimum Notional is 20 USD
 
@@ -80,9 +83,9 @@ def webhook():
         return {
             "code" : "Success",
             "message" : "Order Executed",
-            "Symbol" : str(symbol),
-            "Action" : str(order_action),
-            "Amount" : str(amount),
+            "Symbol" : symbol,
+            "Action" : order_action,
+            "Amount" : amount,
             "Price" : data['bar']['close']
         }
     else:
@@ -90,12 +93,11 @@ def webhook():
         return{
             "code" : "Error",
             "message" : "Order Failed",
-            "Symbol" : str(symbol),
-            "Action" : str(order_action),
-            "Amount" : str(amount),
+            "Symbol" : symbol,
+            "Action" : order_action,
+            "Amount" : amount,
             "Price" : data['bar']['close']
         }
-
 
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
