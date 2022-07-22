384bd527479c749f3990615ca8982ff8622da823
Wisut Kuakittiwong <wisut.kua@gmail.com>
2022-07-22 15:15:57 +0700

    back

--- app.py
+++ app.py
@@ -156,139 +156,139 @@ def webhook():
 ##################################################################################################################
 ##################################################################################################################
 
-@app.route('/webhook2', methods=['POST'])
-def webhook():
-    ##########################################################################
-    # recieve webhook
-    try:
-        wbhook = json.loads(request.data)
-    except Exception as e:
-        print("get webhook error - {}".format(e))
-        return False
+# @app.route('/webhook2', methods=['POST'])
+# def webhook():
+#     ##########################################################################
+#     # recieve webhook
+#     try:
+#         wbhook = json.loads(request.data)
+#     except Exception as e:
+#         print("get webhook error - {}".format(e))
+#         return False
 
-    if wbhook['passphrase'] != config.WEBHOOK_PASSPHRASE_2:
-        return{"message":"invalid passpharse"}
+#     if wbhook['passphrase'] != config.WEBHOOK_PASSPHRASE_2:
+#         return{"message":"invalid passpharse"}
     
-    ##########################################################################
-    # get webhook data
+#     ##########################################################################
+#     # get webhook data
 
-    base_symbol = wbhook['base_symbol']
+#     base_symbol = wbhook['base_symbol']
 
-    symbol = wbhook['symbol']
-    target_symbol = symbol.split(base_symbol)[0]
+#     symbol = wbhook['symbol']
+#     target_symbol = symbol.split(base_symbol)[0]
 
-    side = wbhook['side']
+#     side = wbhook['side']
 
-    buy_ratio_percent = float(wbhook['buy_ratio_percent'])
-    buy_fixed_amount_USDT = float(wbhook['buy_fixed_amount_USDT'])
-    buy_fixed_or_ratio = wbhook['buy_fixed_or_ratio']
+#     buy_ratio_percent = float(wbhook['buy_ratio_percent'])
+#     buy_fixed_amount_USDT = float(wbhook['buy_fixed_amount_USDT'])
+#     buy_fixed_or_ratio = wbhook['buy_fixed_or_ratio']
 
-    sell_ratio_percent = float(wbhook['sell_ratio_percent'])
-    sell_fixed_amount_USDT = float(wbhook['sell_fixed_amount_USDT'])
-    sell_fixed_or_ratio = wbhook['sell_fixed_or_ratio']
+#     sell_ratio_percent = float(wbhook['sell_ratio_percent'])
+#     sell_fixed_amount_USDT = float(wbhook['sell_fixed_amount_USDT'])
+#     sell_fixed_or_ratio = wbhook['sell_fixed_or_ratio']
 
-    # take_profit_percent = float(wbhook['take_profit_percent'])
-    # stop_loss_percent = float(wbhook['stop_loss_percent'])
+#     # take_profit_percent = float(wbhook['take_profit_percent'])
+#     # stop_loss_percent = float(wbhook['stop_loss_percent'])
 
-    # trailing_stop_Type = wbhook['trailing_stop_(no/historical/callback)']
-    # trailing_stop_activation_percent = float(wbhook['trailing_stop_activation_percent'])
-    # trailing_stop_historical_bar = float(wbhook['trailing_stop_historical_bar'])
-    # trailing_stop_callback_rate = float(wbhook['trailing_stop_callback_rate'])
+#     # trailing_stop_Type = wbhook['trailing_stop_(no/historical/callback)']
+#     # trailing_stop_activation_percent = float(wbhook['trailing_stop_activation_percent'])
+#     # trailing_stop_historical_bar = float(wbhook['trailing_stop_historical_bar'])
+#     # trailing_stop_callback_rate = float(wbhook['trailing_stop_callback_rate'])
 
-    ##########################################################################
-    # setup client
+#     ##########################################################################
+#     # setup client
 
-    client = Client(config.API_KEY_2, config.API_SECRET_2)
+#     client = Client(config.API_KEY_2, config.API_SECRET_2)
 
-    ##########################################################################
-    # check account balance free
+#     ##########################################################################
+#     # check account balance free
 
-    account_info= client.get_account()
+#     account_info= client.get_account()
 
-    df_account = pd.DataFrame(account_info['balances'])
+#     df_account = pd.DataFrame(account_info['balances'])
 
-    df_balance = pd.DataFrame()
-    df_balance['asset'] = df_account['asset'].astype(str)
-    df_balance['free'] = df_account['free'].astype(float)
-    df_balance['locked'] = df_account['locked'].astype(float)
+#     df_balance = pd.DataFrame()
+#     df_balance['asset'] = df_account['asset'].astype(str)
+#     df_balance['free'] = df_account['free'].astype(float)
+#     df_balance['locked'] = df_account['locked'].astype(float)
 
-    df_free = df_balance[df_balance['free']>0]
+#     df_free = df_balance[df_balance['free']>0]
     
-    # balance free by symbol
+#     # balance free by symbol
 
-    try:
-        base_balance = float(df_free['free'].loc[df_free['asset'] == base_symbol])
-    except TypeError:
-        target_balance = 0
+#     try:
+#         base_balance = float(df_free['free'].loc[df_free['asset'] == base_symbol])
+#     except TypeError:
+#         target_balance = 0
 
-    try:
-        target_balance = float(df_free['free'].loc[df_free['asset'] == target_symbol])
-    except TypeError:
-        target_balance = 0
+#     try:
+#         target_balance = float(df_free['free'].loc[df_free['asset'] == target_symbol])
+#     except TypeError:
+#         target_balance = 0
 
-    ##########################################################################
-    # get all tick
-    all_price = client.get_all_tickers()
-    df_all_price = pd.DataFrame(all_price)
+#     ##########################################################################
+#     # get all tick
+#     all_price = client.get_all_tickers()
+#     df_all_price = pd.DataFrame(all_price)
 
-    # check target price
-    price = df_all_price[(df_all_price.symbol == symbol)]
-    price = float(price['price'])
+#     # check target price
+#     price = df_all_price[(df_all_price.symbol == symbol)]
+#     price = float(price['price'])
 
-    ##########################################################################
-    # Position Sizing
-    ## BUY
-    if buy_fixed_or_ratio == "fixed":
-        buy_amount = buy_fixed_amount_USDT / price
+#     ##########################################################################
+#     # Position Sizing
+#     ## BUY
+#     if buy_fixed_or_ratio == "fixed":
+#         buy_amount = buy_fixed_amount_USDT / price
 
-    elif buy_fixed_or_ratio == "ratio":
-        buy_amount = (base_balance * buy_ratio_percent) / price
+#     elif buy_fixed_or_ratio == "ratio":
+#         buy_amount = (base_balance * buy_ratio_percent) / price
 
-    else:
-        buy_amount = 0
+#     else:
+#         buy_amount = 0
 
-    ## SELL
-    if sell_fixed_or_ratio == "fixed":
-        sell_amount = sell_fixed_amount_USDT / price
+#     ## SELL
+#     if sell_fixed_or_ratio == "fixed":
+#         sell_amount = sell_fixed_amount_USDT / price
 
-    elif sell_fixed_or_ratio == "ratio":
-        sell_amount = (target_balance * sell_ratio_percent) / 100
+#     elif sell_fixed_or_ratio == "ratio":
+#         sell_amount = (target_balance * sell_ratio_percent) / 100
 
-    else:
-        sell_amount = 0
+#     else:
+#         sell_amount = 0
         
-    ##########################################################################
-    # select order amount
+#     ##########################################################################
+#     # select order amount
 
-    if side == "BUY":
-        order_amount = round(buy_amount,5)
-    elif side == "SELL":
-        order_amount = round(sell_amount,5)
-    else:
-        order_amount = "N/A"
+#     if side == "BUY":
+#         order_amount = round(buy_amount,5)
+#     elif side == "SELL":
+#         order_amount = round(sell_amount,5)
+#     else:
+#         order_amount = "N/A"
 
-    ##########################################################################
-    # order execution
+#     ##########################################################################
+#     # order execution
 
-    order_response = client.create_order(symbol=symbol, side=side, type=ORDER_TYPE_MARKET, quantity=order_amount)
+#     order_response = client.create_order(symbol=symbol, side=side, type=ORDER_TYPE_MARKET, quantity=order_amount)
 
-    if order_response:
-        return {
-            "code" : "Success",
-            "message" : "Order Executed",
-            "symbol" : symbol,
-            "side" : side,
-            "amount" : order_amount
-        }
-    else:
-        print("Order Failed")
-        return{
-            "code" : "Error",
-            "message" : "Order Failed",
-            "symbol" : symbol,
-            "side" : side,
-            "amount" : order_amount
-        }
+#     if order_response:
+#         return {
+#             "code" : "Success",
+#             "message" : "Order Executed",
+#             "symbol" : symbol,
+#             "side" : side,
+#             "amount" : order_amount
+#         }
+#     else:
+#         print("Order Failed")
+#         return{
+#             "code" : "Error",
+#             "message" : "Order Failed",
+#             "symbol" : symbol,
+#             "side" : side,
+#             "amount" : order_amount
+#         }
 
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
